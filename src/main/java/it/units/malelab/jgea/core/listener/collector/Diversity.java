4f8e21cfb6dd48f76b2db2340180fbb110e6ff19
eric <eric@eric-xps13>
2018-06-22 00:11:36 +0200

    IntrinsicDimension collector added.

--- src/main/java/it/units/malelab/jgea/core/listener/collector/Diversity.java
+++ src/main/java/it/units/malelab/jgea/core/listener/collector/Diversity.java
@@ -11,9 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 /**
