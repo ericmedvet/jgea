3b0a7fee56979f15e3b4399a811683a76138510c
Eric Medvet <eric.medvet@gmail.com>
2019-12-28 18:11:08 +0100

    Genetic operators support composition.

--- src/main/java/it/units/malelab/jgea/core/operator/GeneticOperator.java
+++ src/main/java/it/units/malelab/jgea/core/operator/GeneticOperator.java
@@ -5,15 +5,38 @@
  */
 package it.units.malelab.jgea.core.operator;
 
+import it.units.malelab.jgea.core.function.FunctionException;
 import it.units.malelab.jgea.core.function.NonDeterministicFunction;
+import it.units.malelab.jgea.core.listener.Listener;
 import java.util.List;
+import java.util.Random;
 
 /**
  *
  * @author eric
  */
 public interface GeneticOperator<G> extends NonDeterministicFunction<List<G>, List<G>> {
-  
+
   public int arity();
-  
+
+  public default GeneticOperator<G> andThen(GeneticOperator<G> other) {
+    final GeneticOperator<G> thisOperator = this;
+    return new GeneticOperator<G>() {
+      @Override
+      public int arity() {
+        return thisOperator.arity();
+      }
+
+      @Override
+      public List<G> apply(List<G> parents, Random random, Listener listener) throws FunctionException {
+        List<G> intemediate = thisOperator.apply(parents, random, listener);
+        if (intemediate.size() < other.arity()) {
+          throw new IllegalArgumentException(String.format("Cannot apply composed operator: 2nd operator expects %d parents and found %d", other.arity(), intemediate.size()));
+        }
+        return other.apply(intemediate, random, listener);
+      }
+    };
+
+  }
+
 }
