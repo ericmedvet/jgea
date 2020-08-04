fe3389402937b3b7f642d9e723774d3699117034
Eric Medvet <eric.medvet@gmail.com>
2020-08-04 18:03:47 +0200

    More experimenting for SR case.

--- src/main/java/it/units/malelab/jgea/representation/graph/multivariatefunction/MRFGExample.java
+++ src/main/java/it/units/malelab/jgea/representation/graph/multivariatefunction/MRFGExample.java
@@ -21,10 +21,7 @@ import com.google.common.graph.ValueGraph;
 import it.units.malelab.jgea.Worker;
 import it.units.malelab.jgea.core.IndependentFactory;
 import it.units.malelab.jgea.core.Individual;
-import it.units.malelab.jgea.core.evolver.Evolver;
-import it.units.malelab.jgea.core.evolver.RandomSearch;
-import it.units.malelab.jgea.core.evolver.RandomWalk;
-import it.units.malelab.jgea.core.evolver.StandardEvolver;
+import it.units.malelab.jgea.core.evolver.*;
 import it.units.malelab.jgea.core.evolver.stopcondition.FitnessEvaluations;
 import it.units.malelab.jgea.core.evolver.stopcondition.Iterations;
 import it.units.malelab.jgea.core.evolver.stopcondition.TargetFitness;
@@ -63,7 +60,7 @@ public class MRFGExample extends Worker {
   @Override
   public void run() {
     Random r = new Random(1);
-    int maxNodes = 50;
+    int maxNodes = 20;
     SymbolicRegressionProblem p = new Nguyen7(1);
     List<Evolver<ValueGraph<Node, Double>, RealFunction, Double>> evolvers = List.of(
         new RandomSearch<ValueGraph<Node, Double>, RealFunction, Double>(
@@ -135,15 +132,42 @@ public class MRFGExample extends Worker {
             new Worst(),
             100,
             true
+        ),
+        new StandardWithEnforcedDiversity<>(
+            MultivariateRealFunctionGraph.builder()
+                .andThen(GraphBasedRealFunction.builder())
+                .andThen(MathUtils.linearScaler((SymbolicRegressionFitness) p.getFitnessFunction())),
+            new ShallowGraphFactory(0.5d, 0d, 1d, p.getArity(), 1),
+            PartialComparator.from(Double.class).on(Individual::getFitness),
+            100,
+            Map.of(
+                new NodeAddition<>(
+                    BaseFunction.factory(maxNodes, BaseFunction.RE_LU, BaseFunction.GAUSSIAN, BaseFunction.SIN),
+                    Random::nextGaussian
+                ), 2d,
+                new EdgeModification<>((w, random) -> w + random.nextGaussian()), 1d,
+                new EdgeAddition<>(Random::nextGaussian, false), 1d,
+                new EdgeRemoval<>(node -> node instanceof OutputNode), 0.1d,
+                new AlignedCrossover<>(
+                    (w1, w2, random) -> w1 + (w2 - w1) - random.nextDouble(),
+                    node -> node instanceof OutputNode,
+                    false
+                ), 1d
+            ),
+            new Tournament(5),
+            new Worst(),
+            100,
+            true,
+            100
         )
     );
-    evolvers = evolvers.subList(2, 4); //TODO remove
+    evolvers = evolvers.subList(2, evolvers.size()); //TODO remove
     for (Evolver<ValueGraph<Node, Double>, RealFunction, Double> evolver : evolvers) {
       System.out.println(evolver.getClass().getSimpleName());
       try {
         Collection<RealFunction> solutions = evolver.solve(
             Misc.cached(p.getFitnessFunction(), 10000),
-            new TargetFitness<>(0d).or(new FitnessEvaluations(10000)),
+            new TargetFitness<>(0d).or(new FitnessEvaluations(5000)),
             r,
             executorService,
             Listener.onExecutor(listener(
