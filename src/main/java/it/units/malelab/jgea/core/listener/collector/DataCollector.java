679fcf3786de4c0ffc4c596f3383758e5e0e8610
Eric Medvet <eric.medvet@gmail.com>
2020-10-12 23:59:49 +0200

    New method in DataCollector for collecting from any bean (using getters).

--- src/main/java/it/units/malelab/jgea/core/listener/collector/DataCollector.java
+++ src/main/java/it/units/malelab/jgea/core/listener/collector/DataCollector.java
@@ -18,7 +18,14 @@ package it.units.malelab.jgea.core.listener.collector;
 
 import it.units.malelab.jgea.core.listener.Event;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author eric
@@ -27,5 +34,50 @@ import java.util.List;
 public interface DataCollector<G, S, F> {
 
   List<Item> collect(Event<? extends G, ? extends S, ? extends F> event);
+
+  static List<Item> fromBean(Object o, boolean recursive, Map<Class<?>, String> formats, Map<Class<?>, Function<Object, String>> transformers) {
+    List<Item> items = new ArrayList<>();
+    for (Method m : o.getClass().getDeclaredMethods()) {
+      if (m.getParameterCount() > 0) {
+        continue;
+      }
+      if ((m.getModifiers() & Modifier.PUBLIC) == 0) {
+        continue;
+      }
+      if (!m.getName().startsWith("get") && !m.getName().startsWith("is")) {
+        continue;
+      }
+      Class<?> returnType = m.getReturnType();
+      Class<?> matchedFormatType = formats.keySet().stream().filter(returnType::isAssignableFrom).findAny().orElse(null);
+      Class<?> matchedTransformerType = transformers.keySet().stream().filter(returnType::isAssignableFrom).findAny().orElse(null);
+      String name = m.getName().substring(m.getName().startsWith("get") ? 3 : 2);
+      name = name.substring(0, 1).toLowerCase() + name.substring(1);
+      if (matchedFormatType != null) {
+        try {
+          items.add(new Item(
+              name,
+              matchedTransformerType != null ? transformers.get(matchedTransformerType).apply(m.invoke(o)) : m.invoke(o),
+              formats.get(matchedFormatType)
+          ));
+        } catch (IllegalAccessException | InvocationTargetException e) {
+          // ignore
+        }
+      } else if (recursive) {
+        try {
+          Object field = m.invoke(o);
+          if (field != null) {
+            String finalName = name;
+            items.addAll(fromBean(field, recursive, formats, transformers).stream()
+                .map(i -> new Item(finalName + "." + i.getName(), i.getValue(), i.getFormat()))
+                .collect(Collectors.toList())
+            );
+          }
+        } catch (IllegalAccessException | InvocationTargetException e) {
+          // ignore
+        }
+      }
+    }
+    return items;
+  }
 
 }
