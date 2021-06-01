0bd3a8e2852efe68eb80179320015bc868b9b7a3
federicopigozzi <pigozzife@gmail.com>
2021-06-02 00:41:30 +0200

    new elitist selector

---
+++ src/main/java/it/units/malelab/jgea/core/selector/Elitism.java
@@ -0,0 +1,18 @@
+package it.units.malelab.jgea.core.selector;
+
+import it.units.malelab.jgea.core.order.PartiallyOrderedCollection;
+import it.units.malelab.jgea.core.util.Misc;
+
+import java.util.Random;
+
+/**
+ * @author federico
+ */
+public class Elitism implements Selector<Object> {
+
+    @Override
+    public <K> K select(PartiallyOrderedCollection<K> ks, Random random) {
+        return Misc.pickRandomly(ks.firsts(), random);
+    }
+
+}
