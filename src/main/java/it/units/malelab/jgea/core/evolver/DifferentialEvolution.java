d232437d15eb75bb74b7ebde013eaadf4fec3d6b
federicopigozzi <pigozzife@gmail.com>
2021-10-11 10:57:14 +0200

    DE optimization and remap

--- src/main/java/it/units/malelab/jgea/core/evolver/DifferentialEvolution.java
+++ src/main/java/it/units/malelab/jgea/core/evolver/DifferentialEvolution.java
@@ -27,6 +27,7 @@ public class DifferentialEvolution<S, F> extends AbstractIterativeEvolver<List<D
     protected final int populationSize;
     protected final double differentialWeight;
     protected final double crossoverProb;
+    protected final boolean remap;
 
     private static final Logger L = Logger.getLogger(DifferentialEvolution.class.getName());
 
@@ -36,11 +37,13 @@ public class DifferentialEvolution<S, F> extends AbstractIterativeEvolver<List<D
         PartialComparator<? super Individual<List<Double>, S, F>> individualComparator,
         int populationSize,
         double differentialWeight,
-        double crossoverProb) {
+        double crossoverProb,
+        boolean remap) {
       super(solutionMapper, genotypeFactory, individualComparator);
       this.populationSize = populationSize;
       this.differentialWeight = differentialWeight;
       this.crossoverProb = crossoverProb;
+      this.remap = remap;
     }
 
     @Override
@@ -50,31 +53,34 @@ public class DifferentialEvolution<S, F> extends AbstractIterativeEvolver<List<D
 
     @Override
     protected Collection<Individual<List<Double>, S, F>> updatePopulation(PartiallyOrderedCollection<Individual<List<Double>, S, F>> population, Function<S, F> function, Random random, ExecutorService executorService, State state) throws ExecutionException, InterruptedException {
-      List<Individual<List<Double>, S, F>> parents = new ArrayList<>(population.all());
-      Collection<List<Double>> us = new ArrayList<>();
-      for (Individual<List<Double>, S, F> parent : parents) {
+      List<Individual<List<Double>, S, F>> offspring = new ArrayList<>(population.all());
+      List<Integer> indexes = IntStream.range(0, population.size()).boxed().collect(Collectors.toList());
+      int i = 0;
+      for (Individual<List<Double>, S, F> parent : offspring) {
         List<Double> x = parent.getGenotype();
-        List<Double> u = new ArrayList<>(x.size());
-        List<Individual<List<Double>, S, F>> abc = this.pickParents(parents, random);
+        List<Double> trial = new ArrayList<>(x.size());
+        int[] abc = pickParents(indexes, random);
         for (int j = 0; j < x.size(); ++j) {
           if (random.nextDouble() < this.crossoverProb) {
-            u.add(abc.get(0).getGenotype().get(j) + this.differentialWeight * (abc.get(1).getGenotype().get(j) - abc.get(2).getGenotype().get(j)));
+            trial.add(offspring.get(abc[0]).getGenotype().get(j) + this.differentialWeight * (offspring.get(abc[1]).getGenotype().get(j) - offspring.get(abc[2]).getGenotype().get(j)));
           } else {
-            u.add(x.get(j));
+            trial.add(x.get(j));
           }
         }
-        us.add(u);
+        Individual<List<Double>, S, F> trialIndividual = (Individual<List<Double>, S, F>) map(List.of(trial), List.of(), this.solutionMapper, function, executorService, state).get(0);
+        if (this.individualComparator.compare(trialIndividual, parent).equals(PartialComparator.PartialComparatorOutcome.BEFORE)) {
+          offspring.set(i, trialIndividual);
+        }
+        else if (this.remap) {
+          offspring.set(i, map(List.of(), List.of(parent), this.solutionMapper, function, executorService, state).get(0));
+        }
+        ++i;
       }
-      L.fine(String.format("Trials computed: %d individuals", us.size()));
-      Collection<Individual<List<Double>, S, F>> trials = map(us, List.of(), this.solutionMapper, function, executorService, state);
-      L.fine(String.format("Trials evaluated: %d individuals", trials.size()));
-      Collection<Individual<List<Double>, S, F>> offspring = selectPopulation(parents, trials);
-      L.fine(String.format("Population selected: %d individuals", offspring.size()));
+      L.fine(String.format("Trials computed: %d individuals", offspring.size()));
       return offspring;
     }
 
-    protected List<Individual<List<Double>, S, F>> pickParents(List<Individual<List<Double>, S, F>> population, Random random) {
-      List<Integer> indexes = IntStream.range(0, population.size()).boxed().collect(Collectors.toList());
+    protected static int[] pickParents(List<Integer> indexes, Random random) {
       int a = indexes.get(random.nextInt(indexes.size()));
       int b = -1;
       while (b != a) {
@@ -84,22 +90,7 @@ public class DifferentialEvolution<S, F> extends AbstractIterativeEvolver<List<D
       while (c != b) {
         c = indexes.get(random.nextInt(indexes.size()));
       }
-      return List.of(population.get(a), population.get(b), population.get(c));
-    }
-
-    protected Collection<Individual<List<Double>, S, F>> selectPopulation(Collection<Individual<List<Double>, S, F>> parents, Collection<Individual<List<Double>, S, F>> trials) {
-      Collection<Individual<List<Double>, S, F>> offspring = new ArrayList<>();
-      // we know by invariance that each parent is paired with a trial vector at the same index
-      while (parents.iterator().hasNext() && trials.iterator().hasNext()) {
-        Individual<List<Double>, S, F> trial = trials.iterator().next();
-        Individual<List<Double>, S, F> parent = parents.iterator().next();
-        if (this.individualComparator.compare(trial, parent).equals(PartialComparator.PartialComparatorOutcome.BEFORE)) {
-          offspring.add(trial);
-        } else {
-          offspring.add(parent);
-        }
-      }
-      return offspring;
+      return new int[] {a, b, c};
     }
 
 }
