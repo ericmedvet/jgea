a7c4ebf8ae9af28f8f6b4f7cac13e5ef056cff15
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2022-02-10 13:12:47 +0100

    MultiHomogeneousObjectiveProblem added.

---
+++ src/main/java/it/units/malelab/jgea/core/MultiHomogeneousObjectiveProblem.java
@@ -0,0 +1,20 @@
+package it.units.malelab.jgea.core;
+
+import it.units.malelab.jgea.core.order.ParetoDominance;
+import it.units.malelab.jgea.core.order.PartialComparator;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public interface MultiHomogeneousObjectiveProblem<S, O> extends QualityBasedProblem<S, List<O>> {
+
+  List<Comparator<O>> comparators();
+
+  @Override
+  default PartialComparator<List<O>> qualityComparator(){
+    // TODO fix too many new
+    return new ParetoDominance<>(comparators());
+  }
+
+}
