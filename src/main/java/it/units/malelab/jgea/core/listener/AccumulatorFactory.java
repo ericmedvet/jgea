988e2a87be20763637a1d32a8eb13f63893e5485
Eric Medvet <eric.medvet@gmail.com>
2022-02-13 16:24:17 +0100

    New then() method in AccumulatorFactory.

--- src/main/java/it/units/malelab/jgea/core/listener/AccumulatorFactory.java
+++ src/main/java/it/units/malelab/jgea/core/listener/AccumulatorFactory.java
@@ -1,5 +1,23 @@
 package it.units.malelab.jgea.core.listener;
 
+import java.util.function.Function;
+
 public interface AccumulatorFactory<E, O, K> extends ListenerFactory<E, K> {
   Accumulator<E, O> build(K k);
+
+  default <Q> AccumulatorFactory<E, Q, K> then(Function<O, Q> function) {
+    AccumulatorFactory<E, O, K> inner = this;
+    return new AccumulatorFactory<>() {
+      @Override
+      public Accumulator<E, Q> build(K k) {
+        return inner.build(k).then(function);
+      }
+
+      @Override
+      public void shutdown() {
+        inner.shutdown();
+      }
+    };
+  }
+
 }
