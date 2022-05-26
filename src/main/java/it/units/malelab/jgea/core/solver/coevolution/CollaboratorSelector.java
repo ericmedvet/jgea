2dd8dcdd4ea886d4e7476809ae1faa6b0f9022b2
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2022-05-26 08:55:42 +0200

    CollaboratorSelector now selects from a TotallyOrderedCollection.

--- src/main/java/it/units/malelab/jgea/core/solver/coevolution/CollaboratorSelector.java
+++ src/main/java/it/units/malelab/jgea/core/solver/coevolution/CollaboratorSelector.java
@@ -1,6 +1,6 @@
 package it.units.malelab.jgea.core.solver.coevolution;
 
-import it.units.malelab.jgea.core.order.PartiallyOrderedCollection;
+import it.units.malelab.jgea.core.order.TotallyOrderedCollection;
 import it.units.malelab.jgea.core.selector.First;
 import it.units.malelab.jgea.core.selector.Last;
 import it.units.malelab.jgea.core.selector.Random;
@@ -11,11 +11,13 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.random.RandomGenerator;
+import java.util.stream.IntStream;
 
 @FunctionalInterface
 public interface CollaboratorSelector<K> {
 
-  Collection<K> select(PartiallyOrderedCollection<K> ks, RandomGenerator random);
+  Collection<K> select(TotallyOrderedCollection<K> ks, RandomGenerator random);
+
 
   @SuppressWarnings("unchecked")
   static <K> CollaboratorSelector<K> build(String[] collaboratorSelector) {
@@ -89,6 +91,22 @@ public interface CollaboratorSelector<K> {
 
   static <K> CollaboratorSelector<K> lasts() {
     return (ks, random) -> ks.lasts();
+  }
+
+  static <K> CollaboratorSelector<K> topFraction(double fraction) {
+    if (fraction <= 0 || fraction >= 1) {
+      throw new IllegalArgumentException("Illegal fraction specified.");
+    }
+    return (ks, random) -> ks.allOrdered().stream().limit((int) (ks.size() * fraction)).toList();
+  }
+
+  static <K> CollaboratorSelector<K> bottomFraction(double fraction) {
+    if (fraction <= 0 || fraction >= 1) {
+      throw new IllegalArgumentException("Illegal fraction specified.");
+    }
+    return (ks, random) -> IntStream.range(0, (int) (ks.size() * fraction)).mapToObj(
+        i -> ks.allOrdered().get(ks.size() - i - 1)
+    ).toList();
   }
 
 }
