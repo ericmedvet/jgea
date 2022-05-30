dc031fc7f48e4a1ecd4a9b8d6bd172893c4e72d7
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2022-05-30 10:35:35 +0200

    DataBasedSymbolicRegressionProblem updated to read data from file.

--- src/main/java/it/units/malelab/jgea/problem/symbolicregression/DataBasedSymbolicRegressionProblem.java
+++ src/main/java/it/units/malelab/jgea/problem/symbolicregression/DataBasedSymbolicRegressionProblem.java
@@ -17,7 +17,14 @@
 package it.units.malelab.jgea.problem.symbolicregression;
 
 import it.units.malelab.jgea.core.util.Pair;
+import it.units.malelab.jgea.problem.classification.DataUtils;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.util.List;
 
 /**
@@ -34,6 +41,37 @@ public class DataBasedSymbolicRegressionProblem extends SymbolicRegressionProble
         new SymbolicRegressionFitness(trainingData, metric),
         new SymbolicRegressionFitness(validationData, metric)
     );
+  }
+
+  public DataBasedSymbolicRegressionProblem(
+      List<Pair<double[], Double>> data,
+      int folds,
+      int i,
+      SymbolicRegressionFitness.Metric metric
+  ) {
+    this(DataUtils.splitData(data, i, folds).first(), DataUtils.splitData(data, i, folds).second(), metric);
+  }
+
+  public DataBasedSymbolicRegressionProblem(
+      String filename, String yColumnName, int folds, int i, SymbolicRegressionFitness.Metric metric
+  ) throws IOException {
+    this(buildData(filename, yColumnName), folds, i, metric);
+  }
+
+  private static List<Pair<double[], Double>> buildData(String filename, String yColumnName) throws IOException {
+    Reader reader = new FileReader(filename);
+    CSVFormat csvFormat = CSVFormat.Builder.create(CSVFormat.DEFAULT)
+        .setDelimiter(';')
+        .setHeader().setSkipHeaderRecord(true)
+        .build();
+    CSVParser csvParser = csvFormat.parse(reader);
+    List<String> headers = csvParser.getHeaderNames().stream().filter(s -> !s.equals(yColumnName)).toList();
+    List<CSVRecord> records = csvParser.getRecords();
+
+    return records.stream().map(record -> Pair.of(
+        headers.stream().mapToDouble(header -> Double.parseDouble(record.get(header))).toArray(),
+        Double.parseDouble(record.get(yColumnName))
+    )).toList();
   }
 
 }
