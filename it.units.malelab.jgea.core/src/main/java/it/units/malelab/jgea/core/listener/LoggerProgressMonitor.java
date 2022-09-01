9a19ed8ac4b5130a5602865fc3c317b0b21b90e2
Eric Medvet <eric.medvet@gmail.com>
2022-09-02 00:41:18 +0200

    LoggerProgressMonitor added.

---
+++ it.units.malelab.jgea.core/src/main/java/it/units/malelab/jgea/core/listener/LoggerProgressMonitor.java
@@ -0,0 +1,21 @@
+package it.units.malelab.jgea.core.listener;
+
+import it.units.malelab.jgea.core.util.TextPlotter;
+
+import java.util.logging.Logger;
+
+public class LoggerProgressMonitor implements ProgressMonitor {
+
+  private final static Logger L = Logger.getLogger(LoggerProgressMonitor.class.getName());
+
+
+  @Override
+  public void notify(double progress, String message) {
+    L.info(String.format("Progress: %s %s%n", TextPlotter.horizontalBar(progress, 0, 1, 8), message));
+  }
+
+  @Override
+  public void notify(double progress) {
+    notify(progress, "");
+  }
+}
