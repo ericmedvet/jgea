7cee610578703a794cfae63f2264b582a91f83cb
Eric Medvet <eric.medvet@gmail.com>
2022-11-24 18:22:21 +0100

    Starter added.

---
+++ it.units.malelab.jgea.experimenter/src/main/java/it/units/malelab/jgea/experimenter/Starter.java
@@ -0,0 +1,10 @@
+package it.units.malelab.jgea.experimenter;
+
+/**
+ * @author "Eric Medvet" on 2022/11/24 for jgea
+ */
+public class Starter {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
---
+++ it.units.malelab.jgea.experimenter/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * @author "Eric Medvet" on 2022/11/24 for jgea
+ */
+module it.units.malelab.jgea.experimenter {
+  exports it.units.malelab.jgea.experimenter;
+  requires it.units.malelab.jgea.core;
+  requires it.units.malelab.jgea.tui;
+  requires java.logging;
+  requires java.desktop;
+}
