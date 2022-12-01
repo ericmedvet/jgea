ee68f3ffbad204f258895d49146c2a8f2498d7a3
Eric Medvet <eric.medvet@gmail.com>
2022-12-01 16:23:22 +0100

    TotalOrder problem now has a parameter for specifying minimization or maximization.

--- it.units.malelab.jgea.experimenter/src/main/java/it/units/malelab/jgea/experimenter/builders/Problems.java
+++ it.units.malelab.jgea.experimenter/src/main/java/it/units/malelab/jgea/experimenter/builders/Problems.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 eric
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package it.units.malelab.jgea.experimenter.builders;
 
 import it.units.malelab.jgea.core.TotalOrderQualityBasedProblem;
@@ -11,13 +27,16 @@ import java.util.function.Function;
  */
 public class Problems {
 
+  public enum OptimizationType {MINIMIZE, MAXIMIZE}
+
   private Problems() {
   }
 
   @SuppressWarnings("unused")
   public static <S, Q, C extends Comparable<C>> TotalOrderQualityBasedProblem<S, Q> totalOrder(
       @Param("qFunction") Function<S, Q> qualityFunction,
-      @Param(value = "cFunction", dNPM = "ea.f.identity()") Function<Q, C> comparableFunction
+      @Param(value = "cFunction", dNPM = "ea.f.identity()") Function<Q, C> comparableFunction,
+      @Param(value = "type", dS = "minimize") OptimizationType type
   ) {
     return new TotalOrderQualityBasedProblem<>() {
       @Override
@@ -27,6 +46,9 @@ public class Problems {
 
       @Override
       public Comparator<Q> totalOrderComparator() {
+        if (type.equals(OptimizationType.MAXIMIZE)) {
+          return Comparator.comparing(comparableFunction).reversed();
+        }
         return Comparator.comparing(comparableFunction);
       }
     };
