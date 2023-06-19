26564937c53f569fbb02a0c323167b932189f415
Eric Medvet <eric.medvet@gmail.com>
2023-06-19 17:12:40 +0200

    Bug fixed.

--- io.github.ericmedvet.jgea.core/src/main/java/io/github/ericmedvet/jgea/core/representation/grid/DoublesOptionChooser.java
+++ io.github.ericmedvet.jgea.core/src/main/java/io/github/ericmedvet/jgea/core/representation/grid/DoublesOptionChooser.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 eric
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.ericmedvet.jgea.core.representation.grid;
 
 import io.github.ericmedvet.jsdynsym.core.DoubleRange;
@@ -37,7 +53,7 @@ public final class DoublesOptionChooser<T> implements GridDeveloper.Chooser<T> {
       return Optional.empty();
     }
     List<GridGrammar.ReferencedGrid<T>> options = gridGrammar.getRules().get(t);
-    int index = (int) Math.max(Math.round(DoubleRange.UNIT.clip(values.get(i)) * options.size()), options.size() - 1);
+    int index = (int) Math.min(Math.round(DoubleRange.UNIT.clip(values.get(i)) * options.size()), options.size() - 1);
     i = i + 1;
     return Optional.of(options.get(index));
   }
