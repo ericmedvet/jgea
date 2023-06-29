35ec32ad420a0ce5698f1df25adabba0d853abd3
Eric Medvet <eric.medvet@gmail.com>
2023-06-29 11:45:33 +0200

    Maybe found an error.

--- io.github.ericmedvet.jgea.problem/src/main/java/io/github/ericmedvet/jgea/problem/synthetic/Cones.java
+++ io.github.ericmedvet.jgea.problem/src/main/java/io/github/ericmedvet/jgea/problem/synthetic/Cones.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 eric
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.ericmedvet.jgea.problem.synthetic;
 
 import io.github.ericmedvet.jgea.core.problem.MultiHomogeneousObjectiveProblem;
@@ -18,7 +34,7 @@ public class Cones implements MultiHomogeneousObjectiveProblem<List<Double>, Dou
   public Function<List<Double>, List<Double>> qualityFunction() {
     return list -> {
       double r = list.get(0);
-      double h = list.get(0);
+      double h = list.get(0); // TODO should this be 1?
       double s = Math.sqrt(r * r + h * h);
       double lateralSurface = Math.PI * r * s;
       double totalSurface = Math.PI * r * (r + s);
