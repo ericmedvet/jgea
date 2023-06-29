3787d6fbfe16cee06f45ca67a625768da8b6f04d
Eric Medvet <eric.medvet@gmail.com>
2023-06-29 12:29:53 +0200

    Refactoring.

--- io.github.ericmedvet.jgea.problem/src/main/java/io/github/ericmedvet/jgea/problem/grid/CharShapeApproximation.java
+++ io.github.ericmedvet.jgea.problem/src/main/java/io/github/ericmedvet/jgea/problem/grid/CharShapeApproximation.java
@@ -48,71 +48,16 @@ public class CharShapeApproximation implements ComparableQualityBasedProblem<Gri
       }
       int w = ws.get(0);
       int h = ws.size();
-      Grid<Character> grid = Grid.create(w, h, (x, y) -> {
+      target = Grid.create(w, h, (x, y) -> {
         Character c = rows.get(y).get(x);
         return c.equals('·') ? null : c;
       });
-      target = grid;
       this.translation = translation;
     }
   }
 
-  private static Grid.Key center(Grid<?> grid) {
-    return new Grid.Key(
-        (int) grid.entries()
-            .stream()
-            .filter(e -> e.value() != null)
-            .mapToInt(e -> e.key().x())
-            .average()
-            .orElse(0d),
-        (int) grid.entries()
-            .stream()
-            .filter(e -> e.value() != null)
-            .mapToInt(e -> e.key().y())
-            .average()
-            .orElse(0d)
-    );
-  }
-
   @Override
   public Function<Grid<Character>, Double> qualityFunction() {
-    Grid.Key targetCenter = center(target);
-    return grid -> {
-      Grid<Character> thisGrid = grid;
-      Grid<Character> targetGrid = target;
-      if (translation) {
-        //possibly translate
-        Grid.Key thisCenter = center(thisGrid);
-        Grid.Key newCenter = new Grid.Key(
-            Math.max(thisCenter.x(), targetCenter.x()),
-            Math.max(thisCenter.y(), targetCenter.y())
-        );
-        thisGrid = GridUtils.translate(
-            thisGrid,
-            new Grid.Key(newCenter.x() - thisCenter.x(), newCenter.y() - thisCenter.y())
-        );
-        targetGrid = GridUtils.translate(
-            targetGrid,
-            new Grid.Key(newCenter.x() - targetCenter.x(), newCenter.y() - targetCenter.y())
-        );
-      }
-      final Grid<Character> finalThisGrid = thisGrid;
-      return targetGrid.entries().stream()
-          .filter(e -> finalThisGrid.isValid(e.key()))
-          .mapToDouble(e -> {
-            if (e.value() == null && finalThisGrid.get(e.key()) == null) {
-              return 1d;
-            }
-            if (e.value() == null) {
-              return 0d;
-            }
-            if (e.value().equals(finalThisGrid.get(e.key()))) {
-              return 1d;
-            }
-            return 0d;
-          })
-          .average()
-          .orElse(0d);
-    };
+    return grid -> 0d + GridUtils.hammingDistance(grid, target, translation);
   }
 }
