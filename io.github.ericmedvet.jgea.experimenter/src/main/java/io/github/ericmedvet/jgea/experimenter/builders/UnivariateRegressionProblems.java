eb531a55a4e01e0e429fecc6c5f11f3a4969192d
Eric Medvet <eric.medvet@gmail.com>
2023-06-30 11:38:15 +0200

    New Xor problem.

--- io.github.ericmedvet.jgea.experimenter/src/main/java/io/github/ericmedvet/jgea/experimenter/builders/UnivariateRegressionProblems.java
+++ io.github.ericmedvet.jgea.experimenter/src/main/java/io/github/ericmedvet/jgea/experimenter/builders/UnivariateRegressionProblems.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 eric
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.ericmedvet.jgea.experimenter.builders;
 
 import io.github.ericmedvet.jgea.problem.regression.ListNumericalDataset;
@@ -70,6 +86,7 @@ public class UnivariateRegressionProblems {
       case "pagie1" -> new Pagie1(metric);
       case "polynomial4" -> new Polynomial4(metric);
       case "vladislavleva4" -> new Vladislavleva4(metric, seed);
+      case "xor" -> new Xor(metric);
       default -> throw new IllegalArgumentException("Unknown synthetic function: %s".formatted(name));
     };
   }
