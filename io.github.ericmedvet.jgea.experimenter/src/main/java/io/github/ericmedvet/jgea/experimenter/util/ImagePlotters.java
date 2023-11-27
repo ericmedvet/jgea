afd4754ee7c156440646bc949ec8399808acc5db
Eric Medvet <eric.medvet@gmail.com>
2023-11-28 00:02:28 +0100

    Added: new listener for producing a video (of grid-based heatmaps) out of a run with CAbEA.

--- io.github.ericmedvet.jgea.experimenter/src/main/java/io/github/ericmedvet/jgea/experimenter/util/ImagePlotters.java
+++ io.github.ericmedvet.jgea.experimenter/src/main/java/io/github/ericmedvet/jgea/experimenter/util/ImagePlotters.java
@@ -19,8 +19,17 @@
  */
 package io.github.ericmedvet.jgea.experimenter.util;
 
+import io.github.ericmedvet.jsdynsym.core.DoubleRange;
+import io.github.ericmedvet.jsdynsym.grid.Grid;
+import java.awt.*;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
+import java.util.Objects;
 import java.util.function.Function;
+import java.util.logging.Logger;
+import java.util.stream.IntStream;
+import javax.swing.*;
 import org.knowm.xchart.BitmapEncoder;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
@@ -31,7 +40,93 @@ import org.knowm.xchart.style.markers.SeriesMarkers;
 
 public class ImagePlotters {
 
+  private static final Logger L = Logger.getLogger(ImagePlotters.class.getName());
+
   private ImagePlotters() {}
+
+  public static void showImage(BufferedImage image) {
+    EventQueue.invokeLater(() -> {
+      JFrame frame = new JFrame("Image");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.add(new JPanel() {
+        protected void paintComponent(Graphics g) {
+          super.paintComponent(g);
+          Dimension d = getSize();
+          Graphics2D g2d = (Graphics2D) g.create();
+          g2d.drawImage(image, (d.width - image.getWidth()) / 2, (d.height - image.getHeight()) / 2, this);
+          g2d.dispose();
+        }
+
+        public Dimension getPreferredSize() {
+          return new Dimension(image.getWidth(), image.getHeight());
+        }
+      });
+      frame.pack();
+      frame.setLocationRelativeTo(null);
+      frame.setVisible(true);
+    });
+  }
+
+  public static Function<Grid<? extends Number>, BufferedImage> heatMap(
+      int w, int h, Color minColor, Color maxColor, Color nullColor, Color gridColor, double min, double max) {
+    return data -> {
+      BufferedImage image = new BufferedImage(w, h, BufferedImage.TYPE_3BYTE_BGR);
+      Graphics2D g = image.createGraphics();
+      g.setClip(0, 0, w, h);
+      DoubleRange r = new DoubleRange(min, max);
+      // fill with null color
+      g.setColor(nullColor);
+      g.fill(new Rectangle2D.Float(0, 0, w, h));
+      // fill data
+      float cW = (float) w / (float) data.w();
+      float cH = (float) h / (float) data.h();
+      data.entries().forEach(e -> {
+        g.setColor(
+            blend(minColor, maxColor, (float) r.normalize(e.value().floatValue())));
+        float x = (float) e.key().x() / (float) data.w() * (float) w;
+        float y = (float) e.key().y() / (float) data.h() * (float) h;
+        g.fill(new Rectangle2D.Float(x, y, cW, cH));
+      });
+      // draw grid
+      g.setColor(gridColor);
+      IntStream.rangeClosed(0, data.w())
+          .forEach(x -> g.draw(new Line2D.Float((float) x * cW, 0, (float) x * cW, h)));
+      IntStream.rangeClosed(0, data.h())
+          .forEach(y -> g.draw(new Line2D.Float(0, (float) y * cH, w, (float) y * cH)));
+      // return
+      g.dispose();
+      return image;
+    };
+  }
+
+  private static Color blend(Color c1, Color c2, float rate) {
+    rate = (float) DoubleRange.UNIT.clip(rate);
+    float r1 = c1.getRed() / 255f;
+    float g1 = c1.getGreen() / 255f;
+    float b1 = c1.getBlue() / 255f;
+    float r2 = c2.getRed() / 255f;
+    float g2 = c2.getGreen() / 255f;
+    float b2 = c2.getBlue() / 255f;
+    return new Color(r1 + (r2 - r1) * rate, g1 + (g2 - g1) * rate, b1 + (b2 - b1) * rate);
+  }
+
+  public static Function<Grid<? extends Number>, BufferedImage> heatMap(
+      int w, int h, Color minColor, Color maxColor, Color nullColor, Color gridColor) {
+    return data -> {
+      double min = data.values().stream()
+          .filter(Objects::nonNull)
+          .mapToDouble(Number::doubleValue)
+          .min()
+          .orElse(0d);
+      double max = data.values().stream()
+          .filter(Objects::nonNull)
+          .mapToDouble(Number::doubleValue)
+          .max()
+          .orElse(1d);
+      return heatMap(w, h, minColor, maxColor, nullColor, gridColor, min, max)
+          .apply(data);
+    };
+  }
 
   public static Function<XYPlotTable, BufferedImage> xyLines(int w, int h) {
     return data -> {
@@ -58,5 +153,14 @@ public class ImagePlotters {
       data.yNames().forEach(n -> chart.addSeries(n, xs, data.yValues(n)));
       return BitmapEncoder.getBufferedImage(chart);
     };
+  }
+
+  public static Color getColorByName(String name) {
+    try {
+      return (Color) Color.class.getField(name.toUpperCase()).get(null);
+    } catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+      L.warning("Unknown color nome '%s'".formatted(name));
+      return Color.BLACK;
+    }
   }
 }
