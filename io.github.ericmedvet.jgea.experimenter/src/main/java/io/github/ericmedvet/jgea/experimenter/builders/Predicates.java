b4b2c551f09acb32581848e47cfef4e41976ba11
Eric Medvet <eric.medvet@gmail.com>
2023-12-18 23:11:59 +0100

    Added: all() and any() predicates.

--- io.github.ericmedvet.jgea.experimenter/src/main/java/io/github/ericmedvet/jgea/experimenter/builders/Predicates.java
+++ io.github.ericmedvet.jgea.experimenter/src/main/java/io/github/ericmedvet/jgea/experimenter/builders/Predicates.java
@@ -22,53 +22,21 @@ package io.github.ericmedvet.jgea.experimenter.builders;
 import io.github.ericmedvet.jgea.core.listener.NamedFunction;
 import io.github.ericmedvet.jnb.core.Discoverable;
 import io.github.ericmedvet.jnb.core.Param;
+
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 
 @Discoverable(prefixTemplate = "ea.predicate")
 public class Predicates {
-  private Predicates() {}
-
-  @SuppressWarnings("unused")
-  public static <X, T> Predicate<X> in(
-      @Param(value = "f", dNPM = "ea.nf.identity()") NamedFunction<X, T> function,
-      @Param("values") List<T> values) {
-    return x -> values.contains(function.apply(x));
+  private Predicates() {
   }
 
   @SuppressWarnings("unused")
-  public static <X> Predicate<X> lt(
-      @Param(value = "f", dNPM = "ea.nf.identity()") NamedFunction<X, ? extends Number> function,
-      @Param("t") double t) {
-    return x -> function.apply(x).doubleValue() < t;
-  }
-
-  @SuppressWarnings("unused")
-  public static <X> Predicate<X> ltEq(
-      @Param(value = "f", dNPM = "ea.nf.identity()") NamedFunction<X, ? extends Number> function,
-      @Param("t") double t) {
-    return x -> function.apply(x).doubleValue() <= t;
-  }
-
-  @SuppressWarnings("unused")
-  public static <X> Predicate<X> gt(
-      @Param(value = "f", dNPM = "ea.nf.identity()") NamedFunction<X, ? extends Number> function,
-      @Param("t") double t) {
-    return x -> function.apply(x).doubleValue() > t;
-  }
-
-  @SuppressWarnings("unused")
-  public static <X> Predicate<X> gtEq(
-      @Param(value = "f", dNPM = "ea.nf.identity()") NamedFunction<X, ? extends Number> function,
-      @Param("t") double t) {
-    return x -> function.apply(x).doubleValue() >= t;
-  }
-
-  @SuppressWarnings("unused")
-  public static <X, T> Predicate<X> eq(
-      @Param(value = "f", dNPM = "ea.nf.identity()") NamedFunction<X, T> function, @Param("v") T v) {
-    return x -> function.apply(x).equals(v);
+  public static <X> Predicate<X> all(
+      @Param("conditions") List<Predicate<X>> conditions
+  ) {
+    return x -> conditions.stream().allMatch(p -> p.test(x));
   }
 
   @SuppressWarnings("unused")
@@ -77,10 +45,66 @@ public class Predicates {
   }
 
   @SuppressWarnings("unused")
+  public static <X> Predicate<X> any(
+      @Param("conditions") List<Predicate<X>> conditions
+  ) {
+    return x -> conditions.stream().anyMatch(p -> p.test(x));
+  }
+
+  @SuppressWarnings("unused")
+  public static <X, T> Predicate<X> eq(
+      @Param(value = "f", dNPM = "ea.nf.identity()") NamedFunction<X, T> function, @Param("v") T v
+  ) {
+    return x -> function.apply(x).equals(v);
+  }
+
+  @SuppressWarnings("unused")
+  public static <X> Predicate<X> gt(
+      @Param(value = "f", dNPM = "ea.nf.identity()") NamedFunction<X, ? extends Number> function,
+      @Param("t") double t
+  ) {
+    return x -> function.apply(x).doubleValue() > t;
+  }
+
+  @SuppressWarnings("unused")
+  public static <X> Predicate<X> gtEq(
+      @Param(value = "f", dNPM = "ea.nf.identity()") NamedFunction<X, ? extends Number> function,
+      @Param("t") double t
+  ) {
+    return x -> function.apply(x).doubleValue() >= t;
+  }
+
+  @SuppressWarnings("unused")
+  public static <X, T> Predicate<X> in(
+      @Param(value = "f", dNPM = "ea.nf.identity()") NamedFunction<X, T> function,
+      @Param("values") List<T> values
+  ) {
+    return x -> values.contains(function.apply(x));
+  }
+
+  @SuppressWarnings("unused")
+  public static <X> Predicate<X> lt(
+      @Param(value = "f", dNPM = "ea.nf.identity()") NamedFunction<X, ? extends Number> function,
+      @Param("t") double t
+  ) {
+    return x -> function.apply(x).doubleValue() < t;
+  }
+
+  @SuppressWarnings("unused")
+  public static <X> Predicate<X> ltEq(
+      @Param(value = "f", dNPM = "ea.nf.identity()") NamedFunction<X, ? extends Number> function,
+      @Param("t") double t
+  ) {
+    return x -> function.apply(x).doubleValue() <= t;
+  }
+
+  @SuppressWarnings("unused")
   public static <X> Predicate<X> matches(
       @Param(value = "f", dNPM = "ea.nf.identity()") NamedFunction<X, String> function,
-      @Param("regex") String regex) {
+      @Param("regex") String regex
+  ) {
     Pattern p = Pattern.compile(regex);
     return x -> p.matcher(function.apply(x)).matches();
   }
+
 }
