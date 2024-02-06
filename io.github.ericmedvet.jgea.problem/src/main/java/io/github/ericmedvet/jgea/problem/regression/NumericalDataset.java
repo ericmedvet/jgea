ed39a8534ddf51f1ed7ee3b3b4f1cab2ab9eb474
Eric Medvet <eric.medvet@gmail.com>
2024-02-06 12:08:58 +0100

    Added: scaling methods for NumericalDataset.

--- io.github.ericmedvet.jgea.problem/src/main/java/io/github/ericmedvet/jgea/problem/regression/NumericalDataset.java
+++ io.github.ericmedvet.jgea.problem/src/main/java/io/github/ericmedvet/jgea/problem/regression/NumericalDataset.java
@@ -19,13 +19,23 @@
  */
 package io.github.ericmedvet.jgea.problem.regression;
 
+import io.github.ericmedvet.jgea.core.util.Misc;
+import io.github.ericmedvet.jsdynsym.core.DoubleRange;
 import java.util.List;
 import java.util.Map;
 import java.util.function.IntFunction;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public interface NumericalDataset {
+
+  enum Scaling {
+    MIN_MAX,
+    SYMMETRIC_MIN_MAX,
+    STANDARDIZATION
+  }
+
   record Example(double[] xs, double[] ys) {
     public Example(double[] xs, double y) {
       this(xs, new double[] {y});
@@ -41,6 +51,25 @@ public interface NumericalDataset {
           IntStream.range(0, yVarNames.size())
               .mapToObj(i -> Map.entry(yVarNames.get(i), example.ys()[i]))
               .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+    }
+  }
+
+  record VariableInfo(DoubleRange range, double mean, double sd, double q1, double q2, double q3) {
+    static VariableInfo of(List<Double> values) {
+      double mean = values.stream().mapToDouble(v -> v).average().orElseThrow();
+      double sd = Math.sqrt(values.stream()
+          .mapToDouble(v -> (v - mean) * (v - mean))
+          .average()
+          .orElseThrow());
+      return new VariableInfo(
+          new DoubleRange(
+              values.stream().min(Double::compareTo).orElseThrow(),
+              values.stream().max(Double::compareTo).orElseThrow()),
+          mean,
+          sd,
+          Misc.percentile(values, Double::compareTo, 0.25),
+          Misc.percentile(values, Double::compareTo, 0.50),
+          Misc.percentile(values, Double::compareTo, 0.75));
     }
   }
 
@@ -82,5 +111,103 @@ public interface NumericalDataset {
 
   default IntFunction<NamedExample> namedExampleProvider() {
     return i -> new NamedExample(exampleProvider().apply(i), xVarNames(), yVarNames());
+  }
+
+  default NumericalDataset processed(UnaryOperator<Example> processor) {
+    NumericalDataset thisDataset = this;
+    return new NumericalDataset() {
+      @Override
+      public IntFunction<Example> exampleProvider() {
+        return i -> processor.apply(thisDataset.exampleProvider().apply(i));
+      }
+
+      @Override
+      public int size() {
+        return thisDataset.size();
+      }
+
+      @Override
+      public List<String> xVarNames() {
+        return thisDataset.xVarNames();
+      }
+
+      @Override
+      public List<String> yVarNames() {
+        return thisDataset.yVarNames();
+      }
+    };
+  }
+
+  default NumericalDataset scaled(Scaling scaling) {
+    return xScaled(scaling).yScaled(scaling);
+  }
+
+  default String summary() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("n=%d p=%d (p.x=%d p.y=%d)%n"
+        .formatted(
+            size(),
+            xVarNames().size() + yVarNames().size(),
+            xVarNames().size(),
+            yVarNames().size()));
+    sb.append("x vars:\n");
+    xVarNames().forEach(n -> {
+      VariableInfo vi = VariableInfo.of(xValues(n));
+      sb.append("\t%s:\tmin=%.3f\tmax=%.3f\tmean=%.3f\tsd=%.3f\tq1=%.3f\tq2=%.3f\tq3=%.3f%n"
+          .formatted(n, vi.range.min(), vi.range.max(), vi.mean, vi.sd, vi.q1, vi.q2, vi.q3));
+    });
+    sb.append("y vars:\n");
+    yVarNames().forEach(n -> {
+      VariableInfo vi = VariableInfo.of(yValues(n));
+      sb.append("\t%s:\tmin=%.3f\tmax=%.3f\tmean=%.3f\tsd=%.3f\tq1=%.3f\tq2=%.3f\tq3=%.3f%n"
+          .formatted(n, vi.range.min(), vi.range.max(), vi.mean, vi.sd, vi.q1, vi.q2, vi.q3));
+    });
+    return sb.toString();
+  }
+
+  default NumericalDataset xScaled(Scaling scaling) {
+    List<VariableInfo> varInfos =
+        xVarNames().stream().map(n -> VariableInfo.of(xValues(n))).toList();
+    return processed(originalE -> new Example(
+        IntStream.range(0, xVarNames().size())
+            .mapToDouble(j -> switch (scaling) {
+              case MIN_MAX -> varInfos.get(j).range.normalize(originalE.xs[j]);
+              case SYMMETRIC_MIN_MAX -> DoubleRange.SYMMETRIC_UNIT.denormalize(
+                  varInfos.get(j).range.normalize(originalE.xs[j]));
+              case STANDARDIZATION -> (originalE.xs[j] - varInfos.get(j).mean) / varInfos.get(j).sd;
+            })
+            .toArray(),
+        originalE.ys));
+  }
+
+  default List<Double> xValues(String xName) {
+    int xIndex = xVarNames().indexOf(xName);
+    return IntStream.range(0, size())
+        .mapToDouble(i -> exampleProvider().apply(i).xs[xIndex])
+        .boxed()
+        .toList();
+  }
+
+  default NumericalDataset yScaled(Scaling scaling) {
+    List<VariableInfo> varInfos =
+        yVarNames().stream().map(n -> VariableInfo.of(yValues(n))).toList();
+    return processed(originalE -> new Example(
+        originalE.xs,
+        IntStream.range(0, yVarNames().size())
+            .mapToDouble(j -> switch (scaling) {
+              case MIN_MAX -> varInfos.get(j).range.normalize(originalE.ys[j]);
+              case SYMMETRIC_MIN_MAX -> DoubleRange.SYMMETRIC_UNIT.denormalize(
+                  varInfos.get(j).range.normalize(originalE.ys[j]));
+              case STANDARDIZATION -> (originalE.ys[j] - varInfos.get(j).mean) / varInfos.get(j).sd;
+            })
+            .toArray()));
+  }
+
+  default List<Double> yValues(String yName) {
+    int yIndex = yVarNames().indexOf(yName);
+    return IntStream.range(0, size())
+        .mapToDouble(i -> exampleProvider().apply(i).ys[yIndex])
+        .boxed()
+        .toList();
   }
 }
