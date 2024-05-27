45ed745d53dfd3e4cbae6a9c5b5811cd088ff12a
Eric Medvet <eric.medvet@gmail.com>
2024-05-27 22:49:41 +0200

    Working: SpeciatedEvolver updated to new state and individual.

--- io.github.ericmedvet.jgea.core/src/main/java/io/github/ericmedvet/jgea/core/solver/speciation/SpeciatedPOCPopulationState.java
+++ io.github.ericmedvet.jgea.core/src/main/java/io/github/ericmedvet/jgea/core/solver/speciation/SpeciatedPOCPopulationState.java
@@ -19,10 +19,16 @@
  */
 package io.github.ericmedvet.jgea.core.solver.speciation;
 
+import io.github.ericmedvet.jgea.core.order.PartiallyOrderedCollection;
 import io.github.ericmedvet.jgea.core.problem.QualityBasedProblem;
 import io.github.ericmedvet.jgea.core.solver.Individual;
 import io.github.ericmedvet.jgea.core.solver.POCPopulationState;
+import io.github.ericmedvet.jgea.core.solver.State;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * @author "Eric Medvet" on 2023/10/21 for jgea
@@ -30,4 +36,60 @@ import java.util.Collection;
 public interface SpeciatedPOCPopulationState<G, S, Q, P extends QualityBasedProblem<S, Q>>
     extends POCPopulationState<Individual<G, S, Q>, G, S, Q, P> {
   Collection<SpeciatedEvolver.Species<Individual<G, S, Q>>> parentSpecies();
+
+  static <G, S, Q, P extends QualityBasedProblem<S, Q>> SpeciatedPOCPopulationState<G, S, Q, P> empty(
+      P problem, Predicate<State<?, ?>> stopCondition) {
+    return of(
+        LocalDateTime.now(), 0, 0, problem, stopCondition, 0, 0, PartiallyOrderedCollection.from(), List.of());
+  }
+
+  static <G, S, Q, P extends QualityBasedProblem<S, Q>> SpeciatedPOCPopulationState<G, S, Q, P> of(
+      LocalDateTime startingDateTime,
+      long elapsedMillis,
+      long nOfIterations,
+      P problem,
+      Predicate<State<?, ?>> stopCondition,
+      long nOfBirths,
+      long nOfQualityEvaluations,
+      PartiallyOrderedCollection<Individual<G, S, Q>> pocPopulation,
+      Collection<SpeciatedEvolver.Species<Individual<G, S, Q>>> parentSpecies) {
+    record HardState<G, S, Q, P extends QualityBasedProblem<S, Q>>(
+        LocalDateTime startingDateTime,
+        long elapsedMillis,
+        long nOfIterations,
+        P problem,
+        Predicate<State<?, ?>> stopCondition,
+        long nOfBirths,
+        long nOfQualityEvaluations,
+        PartiallyOrderedCollection<Individual<G, S, Q>> pocPopulation,
+        Collection<SpeciatedEvolver.Species<Individual<G, S, Q>>> parentSpecies)
+        implements SpeciatedPOCPopulationState<G, S, Q, P> {}
+    return new HardState<>(
+        startingDateTime,
+        elapsedMillis,
+        nOfIterations,
+        problem,
+        stopCondition,
+        nOfBirths,
+        nOfQualityEvaluations,
+        pocPopulation,
+        parentSpecies);
+  }
+
+  default SpeciatedPOCPopulationState<G, S, Q, P> updatedWithIteration(
+      long nOfNewBirths,
+      long nOfNewQualityEvaluations,
+      PartiallyOrderedCollection<Individual<G, S, Q>> pocPopulation,
+      Collection<SpeciatedEvolver.Species<Individual<G, S, Q>>> parentSpecies) {
+    return of(
+        startingDateTime(),
+        ChronoUnit.MILLIS.between(LocalDateTime.now(), startingDateTime()),
+        nOfIterations() + 1,
+        problem(),
+        stopCondition(),
+        nOfBirths() + nOfNewBirths,
+        nOfQualityEvaluations() + nOfNewQualityEvaluations,
+        pocPopulation,
+        parentSpecies);
+  }
 }
