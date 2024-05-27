35b33263e0cef58b9792370ec9b7ddca34117799
Eric Medvet <eric.medvet@gmail.com>
2024-05-27 18:49:36 +0200

    Working: CAbEA updated to new state and individual.

--- io.github.ericmedvet.jgea.core/src/main/java/io/github/ericmedvet/jgea/core/solver/cabea/GridPopulationState.java
+++ io.github.ericmedvet.jgea.core/src/main/java/io/github/ericmedvet/jgea/core/solver/cabea/GridPopulationState.java
@@ -19,10 +19,17 @@
  */
 package io.github.ericmedvet.jgea.core.solver.cabea;
 
+import io.github.ericmedvet.jgea.core.order.PartialComparator;
+import io.github.ericmedvet.jgea.core.order.PartiallyOrderedCollection;
 import io.github.ericmedvet.jgea.core.problem.QualityBasedProblem;
 import io.github.ericmedvet.jgea.core.solver.Individual;
 import io.github.ericmedvet.jgea.core.solver.POCPopulationState;
+import io.github.ericmedvet.jgea.core.solver.State;
 import io.github.ericmedvet.jnb.datastructure.Grid;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
+import java.util.function.Predicate;
 
 /**
  * @author "Eric Medvet" on 2023/10/28 for jgea
@@ -30,4 +37,60 @@ import io.github.ericmedvet.jnb.datastructure.Grid;
 public interface GridPopulationState<G, S, Q, P extends QualityBasedProblem<S, Q>>
     extends POCPopulationState<Individual<G, S, Q>, G, S, Q, P> {
   Grid<Individual<G, S, Q>> gridPopulation();
+
+  static <G, S, Q, P extends QualityBasedProblem<S, Q>> GridPopulationState<G, S, Q, P> empty(
+      P problem, Predicate<State<?, ?>> stopCondition) {
+    return of(LocalDateTime.now(), 0, 0, problem, stopCondition, 0, 0, Grid.create(0, 0));
+  }
+
+  static <G, S, Q, P extends QualityBasedProblem<S, Q>> GridPopulationState<G, S, Q, P> of(
+      LocalDateTime startingDateTime,
+      long elapsedMillis,
+      long nOfIterations,
+      P problem,
+      Predicate<State<?, ?>> stopCondition,
+      long nOfBirths,
+      long nOfQualityEvaluations,
+      Grid<Individual<G, S, Q>> gridPopulation) {
+    PartialComparator<? super Individual<G, S, Q>> comparator =
+        (i1, i2) -> problem.qualityComparator().compare(i1.quality(), i2.quality());
+    record HardState<G, S, Q, P extends QualityBasedProblem<S, Q>>(
+        LocalDateTime startingDateTime,
+        long elapsedMillis,
+        long nOfIterations,
+        P problem,
+        Predicate<State<?, ?>> stopCondition,
+        long nOfBirths,
+        long nOfQualityEvaluations,
+        PartiallyOrderedCollection<Individual<G, S, Q>> pocPopulation,
+        Grid<Individual<G, S, Q>> gridPopulation)
+        implements GridPopulationState<G, S, Q, P> {}
+    return new HardState<>(
+        startingDateTime,
+        elapsedMillis,
+        nOfIterations,
+        problem,
+        stopCondition,
+        nOfBirths,
+        nOfQualityEvaluations,
+        PartiallyOrderedCollection.from(
+            gridPopulation.values().stream()
+                .filter(Objects::nonNull)
+                .toList(),
+            comparator),
+        gridPopulation);
+  }
+
+  default GridPopulationState<G, S, Q, P> updatedWithIteration(
+      long nOfNewBirths, long nOfNewQualityEvaluations, Grid<Individual<G, S, Q>> gridPopulation) {
+    return of(
+        startingDateTime(),
+        ChronoUnit.MILLIS.between(LocalDateTime.now(), startingDateTime()),
+        nOfIterations() + 1,
+        problem(),
+        stopCondition(),
+        nOfBirths() + nOfNewBirths,
+        nOfQualityEvaluations() + nOfNewQualityEvaluations,
+        gridPopulation);
+  }
 }
